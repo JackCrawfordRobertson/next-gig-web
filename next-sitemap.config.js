ca5db8ea4724f0da263364b2955b8a0e7ab5bf19
Jack Robetson <jackrobertsonps3@gmail.com>
2025-04-08 16:14:19 +0100

    feat: update sitemap configuration and add sitemap-0.xml for improved SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://home.next-gig.co.uk',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://home.next-gig.co.uk', // or next-gig.co.uk for your other subdomain
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  outDir: './public',
+  sourceDir: './.next', // 🔑 tells next-sitemap where to look
+};
